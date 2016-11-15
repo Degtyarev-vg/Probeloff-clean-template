fe7367aab5e5117249e979c47d1e0c29a1d436cc
Vladimir <degtyarev.vg@mail.ru>
2016-11-15 15:21:07 +0600

    Настроена форма с валидацией

--- app/js/common.js
+++ app/js/common.js
@@ -9,27 +9,6 @@ $(document).ready(function() {
 
   //---------------
 
-  // Magnific Popup
-	$('.popup-with-move-anim').magnificPopup({
-	//$('.popup-with-zoom-anim').magnificPopup({
-		type: 'inline',
-
-		fixedContentPos: false,
-		fixedBgPos: true,
-
-		overflowY: 'auto',
-
-		closeBtnInside: true,
-		preloader: false,
-		
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-		//mainClass: 'my-mfp-zoom-in'
-	});
-
-  //---------------
-
   svg4everybody({});
 
   //---------------
